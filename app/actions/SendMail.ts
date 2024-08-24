d286f9b2cb0895b0e0b36500971e342add71c5c2
akem <singhakem03@gmail.com>
2024-08-25 01:14:08 +0530

    ab ni

--- app/actions/SendMail.ts
+++ app/actions/SendMail.ts
@@ -46,8 +46,8 @@ export async function SendMail({
       host: "sandbox.smtp.mailtrap.io",
       port: 2525,
       auth: {
-        user: "",
-        pass: "",
+        user: process.env.MAILTRAP_USER,
+        pass: process.env.MAILTRAP_PASSWORD,
       },
     });
 
