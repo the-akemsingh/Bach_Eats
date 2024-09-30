f516c6a0a96f537093a1bf54d52fd25129a15d48
akem <singhakem03@gmail.com>
2024-09-30 22:49:57 +0530

    yaaar

--- app/(user)/signup/page.tsx
+++ app/(user)/signup/page.tsx
@@ -39,9 +39,9 @@ const Signup = () => {
 
                     </div>
                     <div className="flex flex-col items-center gap-3 mt-8">
-                        <Input value="Name" placeholder="Bachelors" onChange={(e) => setName(e.target.value)} />
-                        <Input value="Email" placeholder="Bachelors@gmail.com" onChange={(e) => setEmail(e.target.value)} />
-                        <Input value="PhoneNumber" placeholder="9876543210" onChange={(e) => setPhonenumber(e.target.value)} />
+                        <Input type="text" value="Name" placeholder="Bachelors" onChange={(e) => setName(e.target.value)} />
+                        <Input type="text" value="Email" placeholder="Bachelors@gmail.com" onChange={(e) => setEmail(e.target.value)} />
+                        <Input type="text" value="PhoneNumber" placeholder="9876543210" onChange={(e) => setPhonenumber(e.target.value)} />
                         <select
                             className="w-full rounded-lg font-mono p-2 text-gray-700 hover:border-yellow-700 bg-gray-100 border shadow-sm"
                             value={gender}
