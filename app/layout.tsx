c2a5a43f2c2e9a6496dc479fb9f47c72448b3e04
akem <singhakem03@gmail.com>
2025-08-06 00:36:18 +0530

    create invite page UI changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <head>
         <style>
           @import url(&apos;https://fonts.googleapis.com/css2?family=Cal+Sans&display=swap&apos;);
-          @import url('https://fonts.googleapis.com/css2?family=Ms+Madi&display=swap');
+          @import url(&apos;https://fonts.googleapis.com/css2?family=Ms+Madi&display=swap&apos;);
         </style>
       </head>
       <body className="">
